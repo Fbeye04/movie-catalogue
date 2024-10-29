34972d74e6612536d88186d144e73a4cebee2597
Fachrezi <107632872+Fbeye04@users.noreply.github.com>
2024-10-29 16:48:45 +0700

    add features built-in database IndexedDB and notication

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,12 +1,14 @@
 import NowPlaying from '../views/pages/now-playing';
 import Upcoming from '../views/pages/upcoming';
 import Detail from '../views/pages/details';
+import Like from '../views/pages/like';
 
 const routes = {
   '/': NowPlaying, // default page
   '/now-playing': NowPlaying,
   '/upcoming': Upcoming,
   '/detail/:id': Detail,
+  '/like': Like,
 };
 
 export default routes;
